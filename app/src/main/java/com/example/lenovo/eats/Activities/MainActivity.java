f8215ffc98d62d7fd69b6511dc8e61ed4d23e838
Abdul Hameed Azeemi <maxer232@gmail.com>
2018-11-23 21:22:00 +0500

    Initial commit

---
+++ app/src/main/java/com/example/lenovo/eats/Activities/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.lenovo.eats.Activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.lenovo.eats.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onComplaintClick(View view)
+    {
+        startActivity(new Intent(this,OrderItems.class));
+    }
+}
